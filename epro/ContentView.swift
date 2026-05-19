b41b282b81ace853a740868594f688a638d1ae82
Henmac <henmac1405@gmail.com>
2026-05-19 10:21:17 +0700

    Initial Commit

---
+++ epro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  epro
+//
+//  Created by user on 19/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ epro/eproApp.swift
@@ -0,0 +1,17 @@
+//
+//  eproApp.swift
+//  epro
+//
+//  Created by user on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct eproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
